e5e6f4ae926cc4efc8b6ef478d7b019e30178d1e
Sumit Bindal <sumit.bindal@stonybrook.edu>
2014-11-05 22:56:00 -0500

    scikit sample

--- ocv-sample.py
+++ ocv-sample.py
@@ -15,9 +15,9 @@ img = cv2.imread(imgPath)
 # Load polygons co-ordinates from .txt
 txtPath = 'path-image-100.seg.000000.000000.txt'
 txt = open(txtPath, 'r')
-polyCnt = 3
-cnt = 0
 
+# Convert polygons to numpy array
+numPoly = []
 for poly in txt:
     polygon = []
     polyList = poly.split(',')
@@ -33,21 +33,10 @@ for poly in txt:
     # Draw a line from every co-ordinate with thickness of 1 px
     for x, y in pairwise(polyList):
         polygon.append([x, y])
-	'''
-        if divmod(cnt, 2):   
-            cv2.line(img, (x1,y1), (x2,y2), (255,0,0), 1)
-        else:
-            cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 1)
-        #cv2.line(img, (x1,y1), (x2,y2), (0,255,0), 1)
-        '''
-    #print polygon
     pts = np.array(polygon, np.int32)
     pts = pts.reshape((-1,1,2))
-    #print pts
+    numPoly.append(pts)
     cv2.polylines(img, [pts], True, (0,255,255))
-    #if cnt == polyCnt:
-        #break
-    #cnt = cnt + 1
 
 # Show image
 cv2.imshow('image', img)
