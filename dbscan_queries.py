1a5463b3206428a742bc42c77b86daaace88ae27
animeshh <animesh.gupta@stonybrook.edu>
2014-11-19 16:52:50 -0500

    dynamic ui changes

--- dbscan_queries.py
+++
@@ -1,9 +0,0 @@
-import plot_dbscan as mydb
-
-mydb.start_dbscan("area100.csv","area100.png")
-mydb.start_dbscan("boundaryIndex.csv","boundaryIndex.png")
-mydb.start_dbscan("path-image-100.seg.000000.000000.csv","allFeatures.png")
-mydb.start_dbscan("area_homogenity.csv","area_homogenity.png")
-mydb.start_dbscan("compactness_correlation.csv","compactness_correlation.png")
-mydb.start_dbscan("permimeter_dissimilarity.csv","permimeter_dissimilarity.png")
-
