29af5fe035f428a238ec63357d51faacab7d0407
Animesh Gupta <animesh.gupta@stonybrook.edu>
2014-11-28 04:02:59 -0500

    updated

--- hackrpi/dynamic_query.py
+++ hackrpi/dynamic_query.py
@@ -1,12 +1,13 @@
-from kmean_cluster import main
-from plot_dbscan import start_dbscan
-
+#from kmean_cluster import main
+#from plot_dbscan import start_dbscan
+from os import getcwd
 #Cluster based on filters
+fileNum = '00'
+dataDir = getcwd() + '/../data/path-image-1' + str(fileNum) + '.tif/'
+ftPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.csv'
+    
 def filteredCluster(paraList):
 
-    fileNum = '00'
-    dataDir = '../data/path-image-1' + str(fileNum) + '.tif/'
-    ftPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.csv'
     fp = open(ftPath,'r')
     filterFeature = dataDir+'filterFeature.csv' 
     fw = open(filterFeature,'w')
@@ -55,19 +56,6 @@ def filteredCluster(paraList):
         i +=1
         if i >1000:
             break
-        """print area
-        print perimeter
-        print compactness
-        print assym
-        print BoundaryIndex
-        print contrast
-        print energy
-        print homogeneity
-        print correlation
-        print dissimilarity
-        print correlation
-        print dissimilarity
-        print asm"""
         
         if ((area >filter_area[0] and area <filter_area[1]) or
             (perimeter >filter_perimeter[0] and perimeter <filter_perimeter[1]) or
@@ -90,115 +78,12 @@ def filteredCluster(paraList):
 
     main("filterFeature.csv","myfeature.png")
     start_dbscan("filterFeature_dbscan.csv","myfeature_dbscan.png")
-        #print paraList
-    """for para in paraList:
-            print para
-            if para[0]=='area':
-                min = para[1]
-                max = para[2]
-                #print Area 
-                if( Area >min and Area < max):
-                    myquery += str(Area) + " "
-                else:
-                    myquery = None
-                    break
-                    
-            elif para[0]=='perimeter':
-                min = para[1]
-                max = para[2]
-                print perimeter
-                if( perimeter >min and perimeter < max):
-                    myquery += str(perimeter) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='compactness':
-                min = para[1]
-                max = para[2]
-                if( compactness >min and compactness < max):
-                    myquery += str(compactness) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='assym':
-                min = para[1]
-                max = para[2]
-                if( assym >min and assym < max):
-                    myquery += str(assym) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='BoundaryIndex':
-                min = para[1]
-                max = para[2]
-                if( BoundaryIndex >min and BoundaryIndex < max):
-                    myquery += str(BoundaryIndex) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='contrast':
-                min = para[1]
-                max = para[2]
-                if( contrast >min and contrast < max):
-                    myquery += str(contrast) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='energy':
-                min = para[1]
-                max = para[2]
-                if( energy >min and energy < max):
-                    myquery += str(energy) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='homogeneity':
-                min = para[1]
-                max = para[2]
-                if( homogeneity >min and homogeneity < max):
-                    myquery += str(homogeneity) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='correlation':
-                min = para[1]
-                max = para[2]
-                if( correlation >min and correlation < max):
-                    myquery += str(correlation) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='dissimilarity':
-                min = para[1]
-                max = para[2]
-                i= f( dissimilarity >min and dissimilarity < max):
-                    myquery += str(dissimilarity) + " "
-                else:
-                    myquery = None
-                    break
-            elif para[0]=='asm':
-                min = para[1]
-                max = para[2]
-                if( ASM >min and ASM < max):
-                    myquery += str(ASM) + " "
-                else:
-                    myquery = None
-                    break
-        if myquery:
-            print("writing data: "+myquery)
-        #fw.write(myquery+"\n")"""
+    
 
-#for testing only comment in real code
-"""area =[0.0,250.0]
-peri = [0.0,250.0]
-compactness = [0.0,150.0]
-assym =[0.0,150.0]
-BoundaryIndex=[0.0,150.0]
-contrast=[0.0,150.0]
-energy=[0.0,150.0]
-homogeneity=[0.0,150.0]
-correlation=[0.0,150.0]
-dissimilarity=[0.0,150.0]
-assym=[0.0,150.0]
-filteredCluster(area,peri,compactness,assym,BoundaryIndex,contrast,energy,
-    homogeneity,correlation,dissimilarity,assym)"""
+def sample_parsing():
+    myfile = open(ftPath,'r')
+    data = myfile.readlines()
+    for line in data:
+        print line
+
+sample_parsing()
