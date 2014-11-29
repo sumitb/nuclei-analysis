e237c875adbb281a497740adb697b10a99c9a89d
animeshh <animesh.gupta@stonybrook.edu>
2014-11-28 22:22:13 -0500

    updated code

---
+++ hackrpi/run.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+from dynamic_query import filteredCluster
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/m')
+def indexm():
+	return render_template('indexm.html')
+@app.route('/ru')
+def ru():
+	return render_template('index.backup.html')
+@app.route('/style')
+def style():
+	return render_template('style.html')
+@app.route('/admin')
+def admin():
+	return render_template('admin.html')
+@app.route('/tables.html')
+def tables():pass
+@app.route('/charts.html')
+def charts():
+	return render_template('charts.html')
+@app.route('/landing')
+def landing():
+	return render_template('landing.html')
+
+@app.route('/feature_select', methods=['POST'])
+def feature_select():
+	print "I'm here!"
+	if request.method == 'POST':
+		print "POST method"
+	featureList = request.form.get('featureList', '')
+	featureList = featureList.split(";")
+	featureList.pop()
+	featureStr = []
+	for flist in featureList:
+		attrib = flist.split(",")
+		print float(attrib[0]), float(attrib[1])
+		featureStr.append([float(attrib[0]), float(attrib[1])])
+	#for f in featureStr:
+	#pass
+	#print f, type(f)
+	#featureValue.append([int(f[0]), int(f[1])])
+	print featureStr
+	filteredCluster(featureStr)
+	#return jsonify(result=a + b)
+	return None
+
+if __name__ == '__main__':
+app.run(debug=True)
