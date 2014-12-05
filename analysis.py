1dfdca449b590aeae5895955243522c67e4685f2
Sumit Bindal <sumit.bindal@stonybrook.edu>
2014-12-05 00:09:25 -0500

    File parser and UI data included

--- analysis.py
+++ analysis.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import itertools
 import math
 
 from skimage.io import *
@@ -11,16 +10,11 @@ from skimage.measure import label, regionprops
 from PIL import Image, ImageDraw
 
 
-fileNum = '01'
-dataDir = 'data/path-image-1' + str(fileNum) + '.tif/'
-
-
-
 def pairwise(iterable):
+    import itertools
     "s -> (s0,s1), (s2,s3), (s4, s5), ..."
     a = iter(iterable)
     return itertools.izip(a, a)
-
 
 #Calculate Perimeter Function
 def calc_peri(coords):
@@ -102,7 +96,6 @@ def bounding_box(coords):
     #return [(min_x,min_y),(max_x,min_y),(max_x,max_y),(min_x,max_y)]
     return (min_x,min_y,min_x+(max_x-min_x),min_y+(max_y-min_y))
 
-
 #Variance
 def variance(list):
    average = sum(list) / len(list)
@@ -113,236 +106,166 @@ def variance(list):
     #    print var
    return var/len(list)
 
-#Cluster based on filters
-"""def filteredCluster(paraList):
-	
-    ftPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.csv'
-    fp = open(ftPath,'r')
-    fw = open(dataDir+'myquery.csv','w')
-    
-    data = fp.readlines()
-    myquery = ""
-    #print "read start"
-    for line in data:
-        value = line.split()
-        Area = float(value[0])
-        perimeter = float(value[1])
-        compactness = float(value[2])
-        assym = float(value[3])
-        BoundaryIndex = float(value[4])
-        contrast = float(value[5])
-        energy = float(value[6])
-        homogeneity = float(value[7])
-        correlation = float(value[8])
-        dissimilarity = float(value[9])
-        ASM = float(value[10])
-        #print "read done"
-        for para in paraList:
-            if para[0]=='Area':
-                min = para[1]
-                max = para[2]
-                #print "inside area" 
-                if( Area >min and Area < max):
-                    myquery += str(Area) + " "
-
-            elif para[0]=='perimeter':
-                min = para[1]
-                max = para[2]
-                #print "inside para"
-                if( perimeter >min and perimeter < max):
-                    myquery += str(perimeter) + " "
-            elif para[0]=='compactness':
-                min = para[1]
-                max = para[2]
-                if( compactness >min and compactness < max):
-                    myquery += str(compactness) + " "
-            elif para[0]=='assym':
-                min = para[1]
-                max = para[2]
-                if( assym >min and assym < max):
-                    myquery += str(assym) + " "
-            elif para[0]=='BoundaryIndex':
-                min = para[1]
-                max = para[2]
-                if( BoundaryIndex >min and BoundaryIndex < max):
-                    myquery += str(BoundaryIndex) + " "
-            elif para[0]=='contrast':
-                min = para[1]
-                max = para[2]
-                if( contrast >min and contrast < max):
-                    myquery += str(contrast) + " "
-            elif para[0]=='energy':
-                min = para[1]
-                max = para[2]
-                if( energy >min and energy < max):
-                    myquery += str(energy) + " "
-            elif para[0]=='homogeneity':
-                min = para[1]
-                max = para[2]
-                if( homogeneity >min and homogeneity < max):
-                    myquery += str(homogeneity) + " "
-            elif para[0]=='correlation':
-                min = para[1]
-                max = para[2]
-                if( correlation >min and correlation < max):
-                    myquery += str(correlation) + " "
-            elif para[0]=='dissimilarity':
-                min = para[1]
-                max = para[2]
-                if( dissimilarity >min and dissimilarity < max):
-                    myquery += str(dissimilarity) + " "
-            elif para[0]=='ASM':
-                min = para[1]
-                max = para[2]
-                if( ASM >min and ASM < max):
-                    myquery += str(ASM) + " "
-        print("writing data: "+myquery)
-        fw.write(myquery+"\n")"""
+# Extract features from image
+def analysis(imgPath, txt, ft):
+    img = imread(imgPath) #set as_grey=True for grayscale
+    # Crop to remove black
+    img = img[:1870, :2340]
+    lx, ly, rgbval = img.shape
         
+    # read image as RGB and add alpha (transparency)
+    im = Image.open(imgPath).convert("L")
+    # convert to numpy (for convenience)
+    imArray = np.asarray(im)
+
+    numPoly = []
+    #polyIdList = [1,2,3,4,5,6]
+    #drawPoly(polyIdList)
+    polyId = 1
+    #print(txt)
+    # Convert polygons to numpy array
+    for line in txt:
+        poly = line.split('\t')
+        poly = poly[51:]
+        poly = poly[0]
+        poly = poly.replace(';', ',')
+
+        mypoly = []
+        polyList = poly.split(',')
+        while '' in polyList:
+            polyList.remove('')
+        if '\n' in polyList:
+            polyList.remove('\n')
+        for i, num in enumerate(polyList):
+            polyList[i] = int(float(polyList[i]))
 
 
-# Load path-image from .jpg
-imgPath = dataDir + 'path-image-1' + str(fileNum) + '.000000.000000.jpg'
+        #x1, y1 = polyList[0], polyList[1]
 
-# Load polygons co-ordinates from .txt
-txtPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.txt'
-txt = open(txtPath, 'r')
-
-# Write features in csv
-ftPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.csv'
-ft = open(ftPath, 'w')
-
-img = imread(imgPath) #set as_grey=True for grayscale
-# Crop to remove black
-img = img[:1870, :2340]
-lx, ly, rgbval = img.shape
-    
-# read image as RGB and add alpha (transparency)
-im = Image.open(imgPath).convert("L")
-# convert to numpy (for convenience)
-imArray = np.asarray(im)
-
-with open(txtPath, 'r') as fin:
-    data = fin.read().splitlines(True)
-with open(txtPath, 'w') as fout:
-    fout.writelines(data[1:])
-
-
-	
-numPoly = []
-#polyIdList = [1,2,3,4,5,6]
-#drawPoly(polyIdList)
-polyId = 1
-#print(txt)
-# Convert polygons to numpy array
-for line in txt:
-    poly = line.split('\t')
-    poly = poly[51:]
-    poly = poly[0]
-    poly = poly.replace(';', ',')
-
-    mypoly = []
-    polyList = poly.split(',')
-    while '' in polyList:
-        polyList.remove('')
-    if '\n' in polyList:
-        polyList.remove('\n')
-    for i, num in enumerate(polyList):
-        polyList[i] = int(float(polyList[i]))
-
-
-    #x1, y1 = polyList[0], polyList[1]
-
-    # Draw a line from every co-ordinate with thickness of 1 px
-    for x, y in pairwise(polyList):
-        mypoly.append((x, y))
-        #polyX.append(x)
-        #polyY.append(y)
+        # Draw a line from every co-ordinate with thickness of 1 px
+        for x, y in pairwise(polyList):
+            mypoly.append((x, y))
+            #polyX.append(x)
+            #polyY.append(y)
+            
+        #pts = np.array(mypoly, np.int32)
+        #pts = pts.reshape((-1,1,2))
         
-    #pts = np.array(mypoly, np.int32)
-    #pts = pts.reshape((-1,1,2))
-    
-    #numPoly.append(pts)
+        #numPoly.append(pts)
 
-    P = np.array(mypoly)
-    # Extract x and y coordinates
-    x = P[:, 0]
-    y = P[:, 1]
+        P = np.array(mypoly)
+        # Extract x and y coordinates
+        x = P[:, 0]
+        y = P[:, 1]
 
-    #1.Area calculation
-    a = x[:-1] * y[1:]
-    b = y[:-1] * x[1:]
-    Area = np.sum(a - b) / 2
-    
-    #5 BoundaryIndex
-    BoundaryIndex = bounding_index(mypoly)
-    #print "Bounding Index: " + str(BoundaryIndex)
-    #for x1,y1 in polygon:
-    # print x1,y1
+        #1.Area calculation
+        a = x[:-1] * y[1:]
+        b = y[:-1] * x[1:]
+        Area = np.sum(a - b) / 2
+        
+        #5 BoundaryIndex
+        BoundaryIndex = bounding_index(mypoly)
+        #print "Bounding Index: " + str(BoundaryIndex)
+        #for x1,y1 in polygon:
+        # print x1,y1
 
-    #BoundingBox
-    #im = Image.open(imgPath) #the size is 500x350
-    box = bounding_box(mypoly)
-    #print "box", box
-    #key = im.crop(box)
-    #key.save("poly_images/polygon" + str(polyId) + ".jpg")
-    #polyId += 1
- 
-    #2.Perimeter
-    perimeter= calc_peri(mypoly)
-    #print "perimeter:",perimeter
-    
-    #3.Compactness
-    compactness= Area/(perimeter*perimeter)
-    #print compactness
+        #BoundingBox
+        #im = Image.open(imgPath) #the size is 500x350
+        box = bounding_box(mypoly)
+        #print "box", box
+        #key = im.crop(box)
+        #key.save("poly_images/polygon" + str(polyId) + ".jpg")
+        #polyId += 1
+     
+        #2.Perimeter
+        perimeter= calc_peri(mypoly)
+        #print "perimeter:",perimeter
+        
+        #3.Compactness
+        compactness= Area/(perimeter*perimeter)
+        #print compactness
 
-    #4.Assymetry
-    xy = x * y
-    #print "var(x)", variance(x)
-    #print "var(y)", variance(y)
-    #print "var(xy)", variance(xy)
-    assym = (2*math.sqrt((0.25*((variance(x)+variance(y))**2))+variance(xy)-(variance(x)*variance(y))))/(variance(x)+variance(y))
-    #print assym 
-    
-    #///////////////////////////////////////////////////////////////////////////////////////////////////
-    #GraylevelCocurrenceMatrix
-    # create mask
-    maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
-    ImageDraw.Draw(maskIm).polygon(mypoly, outline=1, fill=1)
-    mask = np.array(maskIm)
+        #4.Assymetry
+        xy = x * y
+        #print "var(x)", variance(x)
+        #print "var(y)", variance(y)
+        #print "var(xy)", variance(xy)
+        assym = (2*math.sqrt((0.25*((variance(x)+variance(y))**2))+variance(xy)-(variance(x)*variance(y))))/(variance(x)+variance(y))
+        #print assym 
+        
+        #///////////////////////////////////////////////////////////////////////////////////////////////////
+        #GraylevelCocurrenceMatrix
+        # create mask
+        maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
+        ImageDraw.Draw(maskIm).polygon(mypoly, outline=1, fill=1)
+        mask = np.array(maskIm)
 
-    # assemble new image (uint8: 0-255)
-    newImArray = np.empty(imArray.shape,dtype='uint8')
+        # assemble new image (uint8: 0-255)
+        newImArray = np.empty(imArray.shape,dtype='uint8')
 
-    # colors (three first columns, RGB)
-    newImArray[:,:] = imArray[:,:]
+        # colors (three first columns, RGB)
+        newImArray[:,:] = imArray[:,:]
 
-    # transparency (4th column)
-    newImArray[:,:] = mask*255
-    
-    newIm = Image.fromarray(newImArray, "L")#.convert("L")
-    #newIm.save("out"+str(count)+".png")  
+        # transparency (4th column)
+        newImArray[:,:] = mask*255
+        
+        newIm = Image.fromarray(newImArray, "L")#.convert("L")
+        #newIm.save("out"+str(count)+".png")  
 
-    #image_file = Image.open("out"+str(count)+".png").convert("L") # open colour image  
-    #image_file = image_file.convert('1') # convert image to black and white
-    #glcm = greycomatrix(np.asarray(newIm), [1], [0], 256, symmetric=True, normed=True)
-    glcm = greycomatrix(np.asarray(newIm), [1], [0], 256, symmetric=True, normed=True)
+        #image_file = Image.open("out"+str(count)+".png").convert("L") # open colour image  
+        #image_file = image_file.convert('1') # convert image to black and white
+        #glcm = greycomatrix(np.asarray(newIm), [1], [0], 256, symmetric=True, normed=True)
+        glcm = greycomatrix(np.asarray(newIm), [1], [0], 256, symmetric=True, normed=True)
 
-    contrast = greycoprops(glcm, 'contrast').item()
-    energy = greycoprops(glcm, 'energy').item()
-    homogeneity = greycoprops(glcm, 'homogeneity').item()
-    #print('homogeneity is: ',  homogeneity)
-    correlation = greycoprops(glcm, 'correlation').item()
-    #print('correlation is: ',  correlation)
-    dissimilarity = greycoprops(glcm, 'dissimilarity').item()
-    #print('dissimilarity is: ',  dissimilarity)
-    ASM = greycoprops(glcm, 'ASM').item()
-    #print('ASM is: ',  ASM)
+        contrast = greycoprops(glcm, 'contrast').item()
+        energy = greycoprops(glcm, 'energy').item()
+        homogeneity = greycoprops(glcm, 'homogeneity').item()
+        #print('homogeneity is: ',  homogeneity)
+        correlation = greycoprops(glcm, 'correlation').item()
+        #print('correlation is: ',  correlation)
+        dissimilarity = greycoprops(glcm, 'dissimilarity').item()
+        #print('dissimilarity is: ',  dissimilarity)
+        ASM = greycoprops(glcm, 'ASM').item()
+        #print('ASM is: ',  ASM)
 
 
-    #/////////////////////////////////////////////////////////////////////////////////////
-    ft.write(str(polyId)+' '+str(Area) + ' ' + str(perimeter) + ' ' + str(compactness) + ' ' + str(assym) + ' '+str(BoundaryIndex)+ ' '+
-        str(contrast) + ' '+  str(energy)+ ' '  +  str(homogeneity)+ ' '  + str(correlation)+ ' '  + str(dissimilarity)+ ' '  + str(ASM)+"\n")
-    polyId += 1
-ft.close()
-    #/////////////////////////////////////////////////////////////////////////////////////
+        #/////////////////////////////////////////////////////////////////////////////////////
+        ft.write(str(polyId)+' '+str(Area) + ' ' + str(perimeter) + ' ' + str(compactness) + ' ' + str(assym) + ' '+str(BoundaryIndex)+ ' '+
+            str(contrast) + ' '+  str(energy)+ ' '  +  str(homogeneity)+ ' '  + str(correlation)+ ' '  + str(dissimilarity)+ ' '  + str(ASM)+"\n")
+        polyId += 1
+        #/////////////////////////////////////////////////////////////////////////////////////
+    ft.close()
+    return
+
+# Walk directory to obtain images and analyze them
+def fileProcess():
+    import os
+
+    #rootDir = '/data08/images/matlabOutput2'
+    fileNum = '01'
+    dataDir = 'data/path-image-1' + str(fileNum) + '.tif/'
+    rootDir = '/home/sumit/Project/cse595/nuclei/data'
+
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print('Found directory: %s' % dirName)
+        for fname in fileList:
+            print('\t%s' % fname)
+
+            # Load path-image from .jpg
+            imgPath = dataDir + 'path-image-1' + str(fileNum) + '.000000.000000.jpg'
+
+            # Load polygons co-ordinates from .txt
+            txtPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.txt'
+            txt = open(txtPath, 'r')
+
+            # Write features in csv
+            ftPath = dataDir + 'path-image-1' + str(fileNum) + '.seg.000000.000000.csv'
+            ft = open(ftPath, 'w')
+
+            with open(txtPath, 'r') as fin:
+                data = fin.read().splitlines(True)
+            with open(txtPath, 'w') as fout:
+                fout.writelines(data[1:])
+            #analysis(imgPath, txt, ft)
+
+fileProcess()
